3f39d7c0d0a3d0ef833af029b58c304af43ee0d2
Aske Merci <rainman1011@tuta.io>
2021-07-25 10:12:39 +0200

    altered it a bit

--- utils/Networking/Port.py
+++ utils/Networking/Port.py
@@ -5,14 +5,16 @@ import sys
 class Port:
     def __init__(self, host="127.0.0.1", port=None):
         self.platform = sys.platform
-        self.port = str(port)
+        self.port = port
         self.host = host
 
     def is_open(self):
         if self.port is None:
             return Exception('ERR! Please specify a port.')
 
-        if self.platform == "Windows":
-            return os.system("telnet " + self.host + " " + self.port) == 0
+        port = str(self.port)
 
-        return os.system("nc -zw10 " + self.host + " " + self.port) == 0
+        if self.platform == "Windows":
+            return os.system("telnet " + self.host + " " + port) == 0
+
+        return os.system("nc -zw10 " + self.host + " " + port) == 0
